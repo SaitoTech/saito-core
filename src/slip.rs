291394d43942d3ca7e56db4e06980688488697ee
BearGuy <stephen@peterkins.ca>
2019-08-07 14:19:06 +0800

    SlipBody and SlipType introduced in slip primitive

--- src/slip.rs
+++ src/slip.rs
@@ -5,12 +5,13 @@ use crate::crypto::PublicKey;
 pub struct Slip {
     body: SlipBody,
     lc: u8,
+    is_valid: u8,
 }
-
 
 #[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
 pub struct SlipBody {
     add: PublicKey,
+    typ: SlipType,
     amt: u64,
     bid: u32,
     tid: u32,
@@ -18,18 +19,30 @@ pub struct SlipBody {
     bsh: [u8; 32],
 }
 
+#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
+pub enum SlipType{
+  Normal,
+  GoldenTicket,
+  Fee,
+  Rebroadcast,
+  VIP,
+  GoldenChunk,
+}
+
 impl Slip {
     pub fn new(publickey: PublicKey) -> Slip {
         return Slip {
             body: SlipBody {
                 add: publickey,
+                typ: SlipType::Normal,
                 amt: 0,
                 bid: 0,
                 tid: 0,
                 sid: 0,
                 bsh: [0; 32],
             },
-            lc: 0
+            lc: 0,
+            is_valid: 0,
         }
     }
 }

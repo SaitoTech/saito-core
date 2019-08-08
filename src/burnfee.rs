b3563bbcbad829cf64b6bf762c71fe3e9ad205ba
BearGuy <stephen@peterkins.ca>
2019-08-08 13:30:41 +0800

    initial mempool and burnfee calculator

--- src/burnfee.rs
+++ src/burnfee.rs
@@ -1,4 +1,5 @@
 use serde::{Serialize, Deserialize};
+use crate::helper::{create_timestamp};
 
 #[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
 pub struct BurnFee {
@@ -12,3 +13,38 @@ impl BurnFee {
     }
 }
 
+
+#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
+pub struct BurnFeeCalculator {
+    fee: u64,
+    heartbeat: u32,
+    last_block_timestamp: u64,
+    last_block_delta: u64
+}
+
+impl BurnFeeCalculator {
+    pub fn new(self)-> BurnFeeCalculator {
+        return BurnFeeCalculator {
+            fee: 1_000_000_000,
+            heartbeat: 10,
+            last_block_timestamp: create_timestamp(),
+            last_block_delta: 0
+        }
+    }
+
+    pub fn calculate(&self, mut elapsed_time: u64) -> u64 {
+        let elapsed_time_float = elapsed_time as f64 / 1000.0;
+        let heart_beat_float = self.heartbeat as f64 * 2.0;
+
+        if elapsed_time_float > heart_beat_float { return 0; }
+
+        if elapsed_time == 0 { elapsed_time = 1; }
+
+        let elapsed_time_float = elapsed_time as f64;
+        return (self.fee as f64 / (elapsed_time_float / 100_000_000_000 as f64)).round() as u64;
+    }
+
+    pub fn return_current_burnfee(&self) -> u64 {
+        return self.calculate(create_timestamp() - self.last_block_timestamp);
+    }
+}
---
+++ src/mempool.rs
@@ -0,0 +1,46 @@
+use std::{thread, time};
+use crate::block::Block;
+use crate::transaction::Transaction;
+use crate::burnfee::BurnFeeCalculator;
+
+#[derive(Debug)]
+pub struct Mempool {
+    blocks: Vec<Block>,
+    transactions: Vec<Transaction>>,
+    burn_fee_calc: BurnFeeCalculator,
+    work: u64,
+}
+
+impl Mempool {
+    pub fn new() -> Mempool {
+        return (Mempool{
+            blocks: vec![],
+            transactions: vec![],
+        });
+    }
+
+    pub fn add_transaction(&mut self, tx: Transaction) { 
+        self.transactions.push(tx);
+    }
+
+    pub fn return_transaction_length(&self) -> u32 {
+        return self.transactions.len() as u32;
+    }
+
+    pub fn clear_tx_mempool(&mut self) {
+        self.transactions = vec![];
+    }
+
+    pub fn bundle(&self) {
+        loop {
+            // check how much work we have based on tx fees 
+            if burn_fee_calc.return_current_burnfee() > 0 {
+                return;
+            } else {
+                let one_second = time::Duration::from_millis(1000);
+                thread::sleep(one_second);
+                println!("FEE -- {:.8}", burnfee.return_current_burnfee());
+            }
+        }
+    }
+}

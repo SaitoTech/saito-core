0f8905e887cf0a566207ba3e6887b16c9cc06bdd
BearGuy <stephen@peterkins.ca>
2019-08-15 15:51:19 +0800

    decouple config with storage

--- src/storage.rs
+++ src/storage.rs
@@ -5,14 +5,12 @@ use std::str;
 
 use crate::block::Block;
 use crate::helper::create_timestamp;
-use crate::config::Config;
 
 pub const CONFIG_FILENAME: &String= "config.json";
 
 pub struct Storage {
     pub dest: String,
     pub blocks_dir: String,
-    pub config: Config,
 }
 
 impl Storage {
@@ -20,7 +18,6 @@ impl Storage {
         return Storage {
             dest: String::from("data"),
             blocks_dir: String::from("./data/blocks"),
-            config: Config::read_from_file(Path::new(CONFIG_FILENAME)),
         }
     }
 
